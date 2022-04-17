fe8de87d1ade2bc6aeefcffdd928c2c0c927a790
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-04-17 18:08:12 +0200

    checkout and cart comp

--- angular-spa/src/app/app.routing.module.ts
+++ angular-spa/src/app/app.routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { LogoHeaderComponent } from "./components/logo-header/logo-header.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
 
 const routes: Routes = [
   { path: '',   redirectTo: '/products', pathMatch: 'full' }, // redirect to `first-component`
-  { path: 'products', component: LogoHeaderComponent }
+  { path: 'products', component: ProductListComponent },
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent}
 //   { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
