ac040c2243e8b023f5e0c28abbf81e1706def5a9
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-04-17 17:43:43 +0200

    routing module registration

--- angular-spa/src/app/app.module.ts
+++ angular-spa/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { LogoHeaderComponent } from './components/logo-header/logo-header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoHeaderComponent,
+    NavigationComponent,
+    FiltersComponent,
+    ProductListComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
