f7f9f7d806f071cfcedbea04030afcab9811beb5
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-04-22 21:24:16 +0200

    hardcoded data from services

---
+++ angular-spa/src/app/services/product-list.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/resources/models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductListService {
+
+  static readonly apiRoot = "https"
+  private products: Product[] = [
+    {"productId": 1,
+    "name": 'T-shirt',
+    "color": 'red',
+    "size": 'Medium',
+    "quantity": 1,
+    "gender": 'string',
+    "price": 500,
+    "available": 'string',
+    "subcategory": 'string',
+    "image": 'https://cdn.shopify.com/s/files/1/0572/6479/9914/products/spod-1056591552-648-1_480x480@2x.png?v=1625958130',
+    "description": 'string'},
+    {"productId": 2,
+    "name": 'Hoodie',
+    "color": 'black',
+    "size": 'Medium',
+    "quantity": 0,
+    "gender": 'string',
+    "price": 100,
+    "available": 'string',
+    "subcategory": 'string',
+    "image": 'https://cdn.shopify.com/s/files/1/0572/6479/9914/products/spod-1058480633-204-1_300x300.png?v=1644879571',
+    "description": 'string'}
+  ];
+
+  constructor() { }
+
+  getAllProducts() {
+    // call api/search
+    return this.products;
+    }
+
+  getProduct(id: number) {
+    return this.products.find(item => item.productId == id)
+  }
+  
+}
