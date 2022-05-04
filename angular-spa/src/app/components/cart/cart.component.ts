3f221e29e0e8b4b8be561e954888619450c0da75
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-05-04 12:09:25 +0200

    cart component added

--- angular-spa/src/app/components/cart/cart.component.ts
+++ angular-spa/src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/resources/models/product.model';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cartProducts: Product[];
+
+  constructor(private cartSvc: CartService) { }
 
   ngOnInit(): void {
+    this.cartProducts = this.cartSvc.getCartItems();
+    console.log(this.cartProducts);
   }
 
 }
