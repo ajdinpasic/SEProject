f08790c5af0703283959d9f94ed584fb1f6e4692
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-05-19 23:44:10 +0200

    http caller helper

---
+++ angular-spa/src/app/helpers/global.https.ts
@@ -0,0 +1,4 @@
+export class GlobalHttpsCaller {
+    static readonly apiRootLocal = "http://localhost:5000/api/"
+    static readonly apiRootProd = "https://fan-shop-api.herokuapp.com/api/"
+}
--- angular-spa/src/app/services/auth.service.ts
+++ angular-spa/src/app/services/auth.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GlobalHttpsCaller } from '../helpers/global.https';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+     this.http.get(GlobalHttpsCaller.apiRootProd+'search').subscribe((response:any) => {
+      console.log(response)
+    })
+  }
 
   isUserAuthorized() {
     return true;
