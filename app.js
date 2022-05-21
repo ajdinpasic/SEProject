f76b56451887fbf7ad413da52fe700cf4cf57e6e
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-05-21 18:25:41 +0200

    seperated origin added

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const swaggerOptions = {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:4200','https://fan-shop.vercel.app']);
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:4200');
+     res.setHeader('Access-Control-Allow-Origin','https://fan-shop.vercel.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
