bddb13158397d3e37889f9dc822d43b214c9d6e0
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-05-21 18:22:37 +0200

    cors policy for prod added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const swaggerOptions = {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:4200','https://fan-shop.vercel.app']);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
