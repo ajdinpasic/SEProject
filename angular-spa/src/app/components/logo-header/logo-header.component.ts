622505acfa3aae50421c5025af9ad9c71336563e
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-06-18 20:17:31 +0200

    cart counter implemented

--- angular-spa/src/app/components/logo-header/logo-header.component.ts
+++ angular-spa/src/app/components/logo-header/logo-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { ToastrService } from 'ngx-toastr';
+import { CartAddedUtil } from 'src/app/helpers/cart.added.util';
 
 @Component({
   selector: 'app-logo-header',
@@ -14,19 +15,52 @@ export class LogoHeaderComponent implements OnInit {
   constructor(private cartSvc: CartService,private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.count = this.cartSvc.countCartItems();
-    this.cartSvc.cartUpdated.subscribe(
-      (data: boolean) => {
-        if(data) {
-          this.count++
-          this.toastr.success('Product added to your cart!');
-        }
-        else {
-          this.toastr.error('Product quantity out of range!');
-        }
-        
+    this.cartSvc.countCartItems().subscribe(
+      (response) => {
+        this.count = response;
+      }
+    );
+    this.cartSvc.removedFromCartUpdateCounter.subscribe(
+      (response) => {
+         response.subscribe(
+            (response) => {
+              this.count = response;
+            }
+          );
       }
     )
+
+    this.cartSvc.addedItemCartUpdateCounter.subscribe(
+       (response) => {
+         response.subscribe(
+            (response) => {
+              this.count = response;
+            }
+          );
+      }
+    )
+   
+    // this.cartSvc.addedToCart.subscribe(
+    //   (data: boolean) => {
+    //     if(data) {
+    //       this.count++
+    //       this.toastr.success('Product added to your cart!');
+    //     }
+    //     else {
+    //       this.toastr.error('Product quantity out of range!');
+    //     }
+        
+    //   }
+    // )
+
+    // this.cartSvc.removedFromCart.subscribe((data: any) => {
+    //   this.count--;
+    //   // this.toastr.warning('Product removed from your cart!');
+    // })
+
+    // this.cartSvc.addedToCartWithQuantity.subscribe((data: CartAddedUtil)=> {
+    //   this.count+= data.count
+    // })
   }
 
 }
