6a29fcee5a8f023e3e3fde871da5d1a9ca121bc3
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-06-19 15:27:09 +0200

    login form with api added

--- angular-spa/src/app/components/login/login.component.ts
+++ angular-spa/src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { GlobalHttpsCaller } from 'src/app/helpers/global.https';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +16,8 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup
   emailModel : string;
   passwordModel : string;
-  firstNameModel: string;
-  lastNameModel: string;
 
-  constructor(private http: HttpClient, private toastr: ToastrService) { }
+  constructor(private http: HttpClient, private toastr: ToastrService, private router: Router, private authSvc: AuthService) { }
 
   ngOnInit(): void {
      this.loginForm = new FormGroup({
@@ -27,8 +27,27 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form: FormGroup) {
-    	console.log(form)
-     
+    	
+      this.http.post<any>(GlobalHttpsCaller.apiRootLocal+'login',{"email":this.emailModel, "password":this.passwordModel}).subscribe(
+         (response) => {
+           console.log("aaa: "+JSON.stringify(response));
+           if(response.status === 200) {
+            this.toastr.success("Successfully logged in!");
+             this.authSvc.setTokenAuth(response.token);
+            localStorage.setItem("email",response.email)
+
+this.router.navigate(['/products'])
+
+// this.router.navigate(['/products']);
+
+           } else if(response.status === 400) {
+             this.toastr.error("Wrong creditentials!")
+           }
+           else {
+             this.toastr.error("Something went wrong")
+           }
+         }
+       );
   }
 
 }
