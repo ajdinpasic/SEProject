55d551e9391112f50cdaa1cd8158c450a25f4315
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-06-19 15:26:27 +0200

    logout added

--- angular-spa/src/app/components/navigation/navigation.component.ts
+++ angular-spa/src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { GlobalHttpsCaller } from 'src/app/helpers/global.https';
 import { AuthService } from 'src/app/services/auth.service';
 import { NavigationServiceService } from 'src/app/services/navigation-service.service';
 
@@ -11,7 +14,7 @@ import { NavigationServiceService } from 'src/app/services/navigation-service.se
 export class NavigationComponent implements OnInit {
 
    categories: any;
-  constructor(private AuthSvc: AuthService, private navSvc: NavigationServiceService,private toastr: ToastrService) {
+  constructor(private AuthSvc: AuthService, private navSvc: NavigationServiceService,private toastr: ToastrService, private http: HttpClient, private router: Router) {
 
    }
    
@@ -26,6 +29,26 @@ export class NavigationComponent implements OnInit {
       error: (err) => this.toastr.error('No categories to display')
     });
     
+  }
+
+  logout() {
+    let email = localStorage.getItem("email");
+    let token = localStorage.getItem("token");
+     this.http.post<any>(GlobalHttpsCaller.apiRootLocal+'logout',{"email":email, "token":token}).subscribe(
+         (response) => {
+           if(response.status === 200) {
+            this.toastr.success("Successfully logged out!");
+            localStorage.removeItem("email")
+            localStorage.removeItem("token")
+
+this.router.navigate(['/login'])
+
+           }
+           else {
+             this.toastr.error("Something went wrong")
+           }
+         }
+       );
   }
 
 }
